8abd0c8052a58b7bfc64f631e9bc39915c9b94dd
JiashiZhang <jiashi@wustl.edu>
2024-12-18 00:11:15 -0800

    Delay test

--- src/main/java/com/example/reactive_webflux/service/PersonService.java
+++ src/main/java/com/example/reactive_webflux/service/PersonService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+
 @Service
 public class PersonService {
 
@@ -16,7 +18,8 @@ public class PersonService {
     }
 
     public Flux<Person> findAll() {
-        return personRepository.findAll();
+        return personRepository.findAll()
+                .delayElements(Duration.ofSeconds(2)); // 每条数据延迟 2 秒
     }
 
     public Mono<Person> findById(String id) {
